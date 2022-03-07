a84ff53c6fa5b3be8986cfe6e293e8ab0b8458d9
Paulo Hernane <paulo@mobixtec.com>
2022-03-07 19:55:00 -0300

    fix deploy

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import app from "./config/server.js";
 
 import { envs } from "./helpers/utils.js";
 
-app.listen(envs.PORT, (err) => {
+app.listen(envs.PORT, "0.0.0.0", (err) => {
   if (err) {
     console.error(err);
     process.exit(1);
